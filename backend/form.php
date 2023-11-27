0f38c5919216f1cb4aa485afe2dc1ec306873be8
Niko1Akumura <150846893+Niko1Akumura@users.noreply.github.com>
2023-11-27 23:31:56 +0200

    Update form.php

--- backend/form.php
+++ backend/form.php
@@ -75,58 +75,85 @@
             </div>
 
             <div class="form">
-                <form method="post" id="broner">
-                    <div class="name">
-                        <input id="name" type="text" name="name" placeholder="Ваше ім'я">
-                    </div>
+                        <form method="post" action="form.php" id="broner">
+                <div class="name">
+                    <input id="name" type="text" name="name" placeholder="Ваше ім'я">
+                </div>
 
-                    <div class="number">
-                        <select name="number" id="number">
-                            <option value="1">Столик №1</option>
-                            <option value="2">Столик №2</option>
-                            <option value="3">Столик №3</option>
-                            <option value="4">Столик №4</option>
-                            <option value="5">Столик №5</option>
-                            <option value="6">Столик №6</option>
+                <div class="number">
+                    <select name="tables" id="number"> <!-- изменение названия поля на "tables" -->
+                        <option value="1">Столик №1</option>
+                        <!-- ... остальные опции ... -->
+                    </select>
+                </div>
+
+                <div class="num-of-people">
+                    <div class="select-container">
+                        <select name="quantity" id="numOfpeople"> <!-- изменение названия поля на "quantity" -->
+                            <!-- ... опции количества людей ... -->
                         </select>
                     </div>
+                </div>
 
-                    <div class="num-of-people">
-                            <div class="select-container">
-                                <select name="numOfpeople" id="numOfpeople">
-                                    <option value="1">Столик на одного</option>
-                                    <option value="2">Столик на двух</option>
-                                    <option value="3">Солик на трьох</option>
-                                    <option value="4">Столик на чотирьох</option>
-                                    <option value="5">Столик на п'ятьох</option>
-                                    <option value="6">Столик на шісьтьох</option>
-                                </select>
-                            </div>
-                    </div>
+                <div class="phone">
+                    <input id="tel" type="tel" name="phone" placeholder="0980000122"> <!-- изменение названия поля на "phone" -->
+                </div>
 
-                    <div class="phone">
-                        <input id="tel" type="tel" name="tel" placeholder="0980000122">
-                    </div>
+                <div class="data-time">
+                    <label for="data-time"></label>
+                    <input id="datatime" type="datetime-local" name="date"> <!-- изменение названия поля на "date" -->
+                </div>
 
-                    <div class="data-time">
-                        <label for="data-time"></label>
-                        <input id="datatime" type="datetime-local" name="data-time">
-                    </div>
-
-                    <div class="error-submit">
-                        <div class="center-es">
-                            <div class="error">
-                                <span id="alarm">
-
-                                </span>
-                            </div>
-
-                            <div class="submit">
-                                <button type="submit" name="button">ЗАМОВИТИ</button>
-                            </div>
+                <div class="error-submit">
+                    <div class="center-es">
+                        <div class="error">
+                            <span id="alarm"></span>
+                        </div>
+                        <div class="submit">
+                            <button type="submit" name="button">ЗАМОВИТИ</button>
                         </div>
                     </div>
-                </form>
+                </div>
+            </form>
+
+            <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "manhattan-bar";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $tables = intval($_POST['tables']);
+    $quantity = intval($_POST['quantity']);
+    $phone = intval($_POST['phone']);
+    $dateTime = $_POST['date'];
+
+    $sql = "INSERT INTO reservation(`name`, `tables`, `quantity`, `phone`, `date`) VALUES ('$name', $tables, $quantity, $phone, '$dateTime')";
+    $result = mysqli_query($conn, $sql);
+
+    if (!$result) {
+        echo "Ошибка при добавлении записи: " . mysqli_error($conn);
+    } else {
+        echo "Новая запись успешно добавлена";
+    }
+
+    mysqli_close($conn);
+}
+?>
+
+
+
+
+
+
+
             </div>
 
         </div>
